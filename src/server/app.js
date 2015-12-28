c9c8a94a7eb2911c751fee3c561a007857547cd9
mhylle <mhylle@yahoo.com>
2015-12-28 16:51:41 +0100

    added some ui, server is not called yet

---
+++ src/server/app.js
@@ -0,0 +1,101 @@
+/*jshint node:true*/
+'use strict';
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
+//var privateKey  = fs.readFileSync('sslcert/priv.pem', 'utf8');
+//var certificate = fs.readFileSync('sslcert/cert.cer', 'utf8');
+
+//var options = {
+//    key: fs.readFileSync('sslcert/priv.pem'),
+//    cert: fs.readFileSync('sslcert/cert.cer')
+//};
+
+var express = require('express');
+var app = express();
+
+// your express configuration here
+//
+var compression = require('compression');
+var environment = process.env.NODE_ENV;
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var port = process.env.PORT || 8001;
+var helmet = require('helmet');
+var four0four = require('./utils/404')();
+
+var session = require('express-session');
+app.use(compression({filter: shouldCompress}));
+
+var expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
+//app.use(session({
+//    name: 'session',
+//    keys: ['key1', 'key2'],
+//    cookie: {
+//        secure: true,
+//        httpOnly: true,
+//        domain: 'localhost',
+//        path: 'foo/bar',
+//        expires: expiryDate
+//    }
+//}));
+
+app.use(helmet());
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+//
+app.use('/api', require('./routes'));
+
+console.log('About to crank up node');
+console.log('PORT=' + port);
+console.log('NODE_ENV=' + environment);
+
+switch (environment) {
+    case 'build':
+        console.log('** BUILD **');
+        app.use(express.static('./build/'));
+        // Any invalid calls for templateUrls are under app/* and should return 404
+        app.use('/app/*', function (req, res, next) {
+            four0four.send404(req, res);
+        });
+        // Any deep link calls should return index.html
+        app.use('/*', express.static('./build/index.html'));
+        break;
+    default:
+        console.log('** DEV **');
+        app.use(express.static('./src/client/'));
+        app.use(express.static('./'));
+        app.use(express.static('./tmp'));
+        // Any invalid calls for templateUrls are under app/* and should return 404
+        app.use('/app/*', function (req, res, next) {
+            four0four.send404(req, res);
+        });
+        // Any deep link calls should return index.html
+        app.use('/*', express.static('./src/client/index.html'));
+        break;
+}
+
+//var httpsServer = https.createServer(options, app);
+var httpServer = http.createServer(app);
+httpServer.listen(8001, function () {
+    console.log('Express server listening on port ' + port);
+    console.log('env = ' + app.get('env') +
+        '\n__dirname = ' + __dirname +
+        '\nprocess.cwd = ' + process.cwd());
+});
+//httpsServer.listen(443);
+
+function shouldCompress(req, res) {
+    if (req.headers['x-no-compression']) {
+        // don't compress responses with this request header
+        return false;
+    }
+
+    // fallback to standard filter function
+    return compression.filter(req, res);
+}
---
+++ src/server/routes.js
@@ -0,0 +1,102 @@
+/* W0116 */
+var express = require('express');
+var errorhandler = require('errorhandler');
+var router = express.Router();
+
+var four0four = require('./utils/404')();
+
+var app = express();
+
+app.use(errorhandler);
+router.post('/login/', login);
+router.get('/logout/', logout);
+router.get('/*', four0four.notFoundMiddleware);
+
+module.exports = router;
+
+//////////////
+function login(req, res, next) {
+    //var userName = req.body.username;
+    //var passtring = req.body.password;
+    //
+    //var users = userService.getUsers();
+    //var foundUser = false;
+    //if (users === undefined || users === null) {
+    //    res.send(401, 'Invalid Credentials');
+    //    return;
+    //}
+    //var tmpUsers;
+    //if (users.users) {
+    //    tmpUsers = users.users;
+    //} else {
+    //    tmpUsers = users;
+    //}
+    //for (var i = 0; i < tmpUsers.length; i++) {
+    //    var user = tmpUsers[i];
+    //    if (userName === user.username) {
+    //        var userhash = user.hash;
+    //        var salt = user.salt;
+    //        var token = userName;
+    //        var hash = bcrypt.hashSync(passtring, salt);
+    //        if (userhash === undefined) {
+    //            tokens.push(token);
+    //            foundUser = true;
+    //            res.send(200, {accesstoken: token, userName: userName});
+    //            return;
+    //        }
+    //
+    //        var compareSync = bcrypt.compareSync(hash, userhash);
+    //
+    //        if (compareSync) {
+    //            //var expires = new Date();
+    //            //expires.setDate((new Date()).getDate() + 5);
+    //
+    //            tokens.push(token);
+    //            foundUser = true;
+    //            res.send(200, {accesstoken: token, userName: userName});
+    //            return;
+    //        }
+    //    }
+    //}
+    //res.send(401, 'Invalid Credentials');
+}
+
+function logout(req, res, description) {
+    //var token = req.headers.accesstoken;
+    //removeFromTokens(token);
+    //res.send(200);
+}
+
+function removeFromTokens(token) {
+    //for (var counter = 0; counter < tokens.length; counter++) {
+    //    if (tokens[counter] === token) {
+    //        tokens.splice(counter, 1);
+    //        break;
+    //    }
+    //}
+}
+
+function requiresAuthentication(request, response, next) {
+    ////console.log(request.headers);
+    //if (request.headers.accesstoken && request.headers.accesstoken !== null) {
+    //    var token = request.headers.accesstoken;
+    //    // underscore checks if the token supplied is in the list of tokens that the server is aware of.
+    //    // if the token is found we can then start looking into if it is a valid one.
+    //    var where = _.where(tokens, token);
+    //    if (where.length > 0) {
+    //        //var decodedToken = jwt.decode(token, secret);
+    //        console.log(token);
+    //        next();
+    //        return;
+    //        //if (new Date(decodedToken.expires) > new Date()) {
+    //        //    next();
+    //        //    return;
+    //        //} else {
+    //        //    removeFromTokens();
+    //        //    response.status(401).send('Your session is expired');
+    //        //    return;
+    //        //}
+    //    }
+    //}
+    //response.status(401).send('No access token found in the request');
+}
